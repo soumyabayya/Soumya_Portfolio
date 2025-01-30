c199e5e264d40fa784ae496ed28854ed3d678c95
soumyabayya <149183133+soumyabayya@users.noreply.github.com>
2025-01-30 14:00:57 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,73 @@
+var typed = new Typed(".multiple-text", {
+  strings: [
+    "FrontEnd Development ",
+    "Backend Development",
+
+   
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    document.querySelectorAll(".Projects-section").forEach(function (section) {
+      if (isElementInViewport(section)) {
+        section.classList.remove("animated");
+      } else {
+        section.classList.add("animated");
+      }
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    document.querySelectorAll(".AboutMe").forEach(function (section) {
+      if (isElementInViewport(section)) {
+        section.classList.remove("animated");
+      } else {
+        section.classList.add("animated");
+      }
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    document.querySelectorAll(".Skills-section").forEach(function (section) {
+      if (isElementInViewport(section)) {
+        section.classList.remove("animated");
+      } else {
+        section.classList.add("animated");
+      }
+    });
+  });
+});
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function song() {
+  open("https://github.com/soumyabayya/AIML-2024-25/blob/main/AIML_PROJECT.ipynb");
+}
+function vote() {
+  open("https://github.com/soumyabayya/Python_Project");
+}
+
+function resume() {
+  open(
+    "https://drive.google.com/file/d/1HWr7Rm0hpqaFvg9pjPImJ2TPzDKA0ScY/view?usp=sharing"
+  );
+}
